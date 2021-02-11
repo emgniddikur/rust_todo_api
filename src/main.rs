d321226e7753314921d1d477d2934c3e3f7e1b9f
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-02-12 00:45:15 +0900

    Hello, world!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, middleware, App, HttpServer};
+
+#[get("/")]
+async fn index() -> String {
+    "Hello, world!".to_string()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::DefaultHeaders::new().header("X-Version", "0.2"))
+            .wrap(middleware::Compress::default())
+            .service(index)
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
